507228f103340f1866b629e02bae754f5a09355d
ssskram <paul.marks@pittsburghpa.gov>
2017-11-25 10:04:30 -0500

    cleaned out account controler action methods

--- Extensions/UrlHelperExtensions.cs
+++ Extensions/UrlHelperExtensions.cs
@@ -16,14 +16,5 @@ namespace Microsoft.AspNetCore.Mvc
                 values: new { userId, code },
                 protocol: scheme);
         }
-
-        public static string ResetPasswordCallbackLink(this IUrlHelper urlHelper, string userId, string code, string scheme)
-        {
-            return urlHelper.Action(
-                action: nameof(AccountController.ResetPassword),
-                controller: "Account",
-                values: new { userId, code },
-                protocol: scheme);
-        }
     }
 }
