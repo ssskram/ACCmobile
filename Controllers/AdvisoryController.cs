c88c5e01416999cca676e2162754f5c0f5eceef7
ssskram <paul.marks@pittsburghpa.gov>
2017-12-17 18:52:37 -0500

    rest post workin

--- Controllers/AdvisoryController.cs
+++ Controllers/AdvisoryController.cs
@@ -31,35 +31,17 @@ namespace ACCmobile.Controllers
         }
         static async Task Execute(AdvisoryGeneralInfo model)
         {
-            var apiKey = Environment.GetEnvironmentVariable("sendgrid");
-            var client = new SendGridClient(apiKey);
-            var from = new EmailAddress("HelpDesk@azurewebservices.us", "Help Desk");
-            var subject = model.Address;
-            var to = new EmailAddress("paulmarks211@gmail.com", "Paul Marks");
-            var plainTextContent = 
-                String.Format
-                ("<strong>A new advisory form has been submitted.</strong> <br><br> The user's name is {0} {1}, <br><br> The user's email address is {2} <br><br> Phone number  = {3} <br><br> Reason for visit = {4} <br><br> ADV. PGH. Code  = {5} <br><br> Citation number = {6} <br><br> Comments = {7}",
-                         model.OwnersFirstName, // 0
-                         model.OwnersLastName, // 1
-                         model.Address, // 2
-                         model.OwnersTelephoneNumber, // 3
-                         model.ReasonForVisit, // 4
-                         model.PGHCode, // 5
-                         model.CitationNumber, // 6 
-                         model.Comments); // 7
-            var htmlContent = 
-                String.Format
-                ("<strong>A new advisory form has been submitted.</strong> <br><br> The user's name is {0} {1}, <br><br> The user's email address is {2} <br><br> Phone number  = {3} <br><br> Reason for visit = {4} <br><br> ADV. PGH. Code  = {5} <br><br> Citation number = {6} <br><br> Comments = {7}",
-                         model.OwnersFirstName, // 0
-                         model.OwnersLastName, // 1
-                         model.Address, // 2
-                         model.OwnersTelephoneNumber, // 3
-                         model.ReasonForVisit, // 4
-                         model.PGHCode, // 5
-                         model.CitationNumber, // 6 
-                         model.Comments); // 7
-            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
-            var response = await client.SendEmailAsync(msg);
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            var stringTask = client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
+
+            var msg = await stringTask;
+            Console.Write(msg);
+
         }
     }
 }
