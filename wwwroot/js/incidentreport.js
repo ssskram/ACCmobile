3c6f977f181a9e8c6916f9cc20a117382d172f85
ssskram <paul.marks@pittsburghpa.gov>
2018-02-08 14:28:18 -0500

    continued formatting incident report

--- wwwroot/js/incidentreport.js
+++ wwwroot/js/incidentreport.js
@@ -13,14 +13,16 @@ function initMap() {
 function geocodeAddress(geocoder, resultsMap) {
     infoWindow = new google.maps.InfoWindow;
     var address = $ ( "#address" ).text();
+    var incidentid = $ ( "#id" ).text();
+    var content = "<b>" + address + "</b> <br/>Incident ID:<br/>" + incidentid;
     geocoder.geocode({'address': address}, function(results, status) {
       if (status === 'OK') {
         resultsMap.setCenter(results[0].geometry.location);
         infoWindow.setPosition(results[0].geometry.location);
-        infoWindow.setContent(address);
+        infoWindow.setContent(content);
         infoWindow.open(resultsMap);
       } else {
         alert('Geocode was not successful for the following reason: ' + status);
       }
-    });
+    })
 }
