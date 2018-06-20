6b5f71cdb6233984ef67dc96c0f243b11cea35b5
ssskram <paul.marks@pittsburghpa.gov>
2018-06-20 16:07:51 -0400

    first to spa

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using accmobile.Models;
+
+namespace accmobile.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
