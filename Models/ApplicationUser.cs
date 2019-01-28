2c5d5e9d5777659211ffe79a0cb7634948134ed3
ssskram <paul.marks@pittsburghpa.gov>
2019-01-28 10:16:28 -0500

    first node

--- Models/ApplicationUser.cs
+++
@@ -1,5 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace accmobile.Models {
-    public class ApplicationUser : IdentityUser { }
-}
--- Models/Survey.cs
+++
@@ -1,8 +0,0 @@
-namespace accmobile.Models {
-    public class Survey {
-        public string name { get; set; }
-        public string department { get; set; }
-        public string body { get; set; }
-        public string futureUserTesting { get; set; }
-    }
-}
