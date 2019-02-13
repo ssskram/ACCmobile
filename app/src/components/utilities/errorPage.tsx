968df161455fdc7f6247e135fcbee25502f78a2c
ssskram <paul.marks@pittsburghpa.gov>
2019-02-13 15:14:59 -0500

    post is working correctly

--- app/src/components/utilities/errorPage.tsx
+++ app/src/components/utilities/errorPage.tsx
@@ -1,12 +1,15 @@
 import * as React from 'react'
+import { Cat } from 'react-kawaii'
 
 export default class Error extends React.Component<any, any> {
 
     public render() {
         return (
-            <div className='text-center'>
+            <div className='text-center' style={{ marginTop: '50px' }}>
+                <Cat size={300} mood="shocked" color="#AED3E5" />
                 <h1>Uh oh!</h1>
-                <h3>That didn't work.  Please log out, log back in, and try again.</h3>
+                <h1>That didn't work.</h1>
+                <h3>Please log out, log back in, and try again.</h3>
                 <h3>If this problem persists, please contact the system administrator.</h3>
             </div>
 
